4eddd810a1fb0cb87eefe079f71ef86291e3aa0a
Kantha Vishwas Gowda <kgowda@ncsu.edu>
2022-09-10 20:20:26 +0000

    simulated_AND

---
+++ Simulated_AND.py
@@ -0,0 +1,9 @@
+from dimod import ExactSolver
+sampler = ExactSolver() 
+
+Q = {('x', 'y'): 1, ('x', 'z'): -2, ('y', 'z'): -2, ('z', 'z'): 3}
+
+response = sampler.sample_qubo(Q)
+print (response)
+for datum in response.data('sample', 'energy’):
+    print(datum.sample, datum.energy)
