c34a0bfa968d815e2ca177b1267c53ef4357b7d5
Kantha Vishwas Gowda <kgowda@ncsu.edu>
2022-09-11 19:28:41 +0000

    Simulated_NAND

---
+++ Simulated_NAND.py
@@ -0,0 +1,10 @@
+#Simulated NAND
+from dimod import ExactSolver
+sampler = ExactSolver() 
+
+Q = {('x', 'x'): -2, ('y', 'y'): -2, ('z', 'z'): -3, ('x', 'y'): 1, ('x', 'z'): 2, ('y', 'z'): 2}
+
+response = sampler.sample_qubo(Q)
+#print (response)
+for datum in response.data(['sample', 'energy']):   
+  print(datum.sample, "Energy: ", datum.energy)
